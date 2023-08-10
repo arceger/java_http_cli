c4718dfb8d02ce6db8ea979ff9887fac7beb0917
Gerson Arce <arceger@hotmail.com>
2023-08-10 19:52:23 -0300

    correcao_credential

--- src/App.java
+++ src/App.java
@@ -1,19 +1,26 @@
+import java.io.FileReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        FileReader reader = new FileReader(".env");
+        Properties p = new Properties();
+        p.load(reader);
+
         // Defina as credenciais de autenticação
-        String username = "user";
-        String password = "$enha";
+        String username = p.getProperty("username");
+        String password = p.getProperty("password");
         
         // Codifique as credenciais em Base64
         String credentials = username + ":" + password;
-       // String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
         
         // Crie um cliente HTTP
         HttpClient httpClient = HttpClient.newHttpClient();
@@ -21,7 +28,7 @@ public class App {
         // Construa a requisição com a autenticação Basic Auth
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI("http://localhost:8080/rst"))
-                //.header("Authorization", "Basic " + encodedCredentials)
+                .header("Authorization", "Basic " + encodedCredentials)
                 .build();
         
         // Envie a requisição e obtenha a resposta
